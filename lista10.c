8049635cd0693f3e63722ebbf4e3536a435155ad
Matheus Concon <142353640+goldjkk@users.noreply.github.com>
2024-05-24 21:24:02 -0300

    Create lista10.c

---
+++ lista10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+
+void funcaoParaTestar() {
+    // Simulação de uma função que leva tempo para ser executada
+    for (long long i = 0; i < 1000000000; i++);
+}
+
+int main() {
+    clock_t inicio, fim;
+    double tempoGasto;
+
+    inicio = clock();
+
+    funcaoParaTestar();
+
+    fim = clock();
+
+    tempoGasto = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+    printf("Tempo de execução: %f segundos\n", tempoGasto);
+
+    return 0;
+}
